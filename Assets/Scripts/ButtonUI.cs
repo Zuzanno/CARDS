f9dd1c91a5480125c17a33f7a1f87d112e39b8ef
Zuzanno <marindiaz1234@gmail.com>
2020-04-05 00:35:43 -0500

    Release

---
+++ Assets/Scripts/ButtonUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonUI : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color highlightColor = Color.green;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void OnMouseEnter()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(.5f, .5f, .5f);
+    }
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CARDS
+ This is a 2D cards game made using assets I found online
